286ce3b611c0954675dfede6a3d3fdecdaaed1bf
Kudadonbe <hussain.shareef@gmail.com>
2025-01-27 09:37:58 +0500

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Hussain Shareef on 27-01-2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
